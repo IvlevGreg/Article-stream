d68aa2de8d2407215fb22bad51804ce38afc02d5
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-14 18:29:37 +0300

    refactoring: implemente fsd architecture
    just moved the files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './App';
+import { App } from './app/App';
 
-import './styles/index.scss';
-import ThemeProvider from './theme/ThemeProvider';
+import './app/styles/index.scss';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
 
 render(
   <BrowserRouter>
