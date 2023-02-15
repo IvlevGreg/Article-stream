bf299d07c7032a6cf066226b4df10296b0b4d359
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-15 17:21:48 +0300

    feat: import i18n

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 
 import './app/styles/index.scss';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import './shared/config/i18n/i18n';
 
 render(
   <BrowserRouter>
