521d9249a4d58741bd6ea9596c9ac1cda829e6c9
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-15 10:49:53 +0300

    feat: add DefinePlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 export function buildPlugins({
   paths,
+  isDev,
 }: BuildOptions): Array<webpack.WebpackPluginInstance> {
   return [
     new htmlWebpackPlugin({
@@ -15,6 +16,9 @@ export function buildPlugins({
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
     }),
   ];
 }
