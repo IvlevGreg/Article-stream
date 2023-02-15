08fe3fd8832ee730f95a2259e6ae8b11ce6d1e9f
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-15 17:20:54 +0300

    feat: create applink ui component

---
+++ src/shared/ui/Applink/AppLink.tsx
@@ -0,0 +1,35 @@
+import { FC, PropsWithChildren } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './AppLink.module.scss';
+
+export enum AppLinkTheme {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+}
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (
+  props: PropsWithChildren<AppLinkProps>
+) => {
+  const {
+    to,
+    className,
+    children,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
+
+  return (
+    <Link
+      {...otherProps}
+      to={to}
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+    >
+      {children}{' '}
+    </Link>
+  );
+};
