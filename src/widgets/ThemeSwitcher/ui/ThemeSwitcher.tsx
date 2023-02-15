6340e9af00c296d743a31231c1b8cc894ac2ef78
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-15 17:19:12 +0300

    feat: create theme switcher button

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './ThemeSwitcher.module.scss';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Button, ThemeButton } from 'shared/ui';
+
+interface ThemeSwitcherProps {
+  className?: string;
+}
+
+export function ThemeSwitcher(props: ThemeSwitcherProps) {
+  const { className } = props;
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      onClick={toggleTheme}
+      className={classNames(cls.ThemeSwitcher, {}, [className])}
+    >
+      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+    </Button>
+  );
+}
