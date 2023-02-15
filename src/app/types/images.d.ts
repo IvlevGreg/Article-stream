e362b703174d60592cf49a8b012f7834339f2e7d
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-15 10:50:30 +0300

    feat: create new global ts types

---
+++ src/app/types/images.d.ts
@@ -0,0 +1,19 @@
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.pjpg' {
+  const value: string;
+  export default value;
+}
