201f39291fe3d25c9ebf27ec101915b8cb445d6a
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-15 17:23:12 +0300

    refactoring: add default values for variables

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,13 @@
 type Mods = Record<string, boolean | string>;
 
-export function classNames(cls: string, mods: Mods, additional: string[]) {
+export function classNames(
+  cls: string,
+  mods: Mods = {},
+  additional: string[] = []
+) {
   return [
     cls,
-    ...additional,
+    ...additional.filter(Boolean),
     Object.entries(mods)
       .filter(([className, value]) => !!value)
       .map(([className]) => className),
