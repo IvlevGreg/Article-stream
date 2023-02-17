9fc49937eadb1499fc0fe916c86b3d20923c0cce
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-17 08:30:51 +0300

    feat: create not found page component

--- src/shared/config/route/routeConfig.tsx
+++ src/shared/config/route/routeConfig.tsx
@@ -1,18 +1,23 @@
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { NotFoundPage } from 'pages/NotFoundPage/ui/NotFoundPage';
 import { type RouteProps } from 'react-router-dom';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOTFOUND = 'notFound',
+
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOTFOUND]: '/*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: { path: RoutePath.main, element: <MainPage /> },
   [AppRoutes.ABOUT]: { path: RoutePath.about, element: <AboutPage /> },
+  [AppRoutes.NOTFOUND]: { path: RoutePath.notFound, element: <NotFoundPage /> },
 };
