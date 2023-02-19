bca0d8d3d316f0017d697a6b810bfdfbe3ebd873
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-19 11:49:44 +0300

    feat: create page for error boundary errors

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,27 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ThemeButton } from 'shared/ui';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export function PageError(props: PageErrorProps) {
+  const { className } = props;
+  const { t } = useTranslation();
+
+  function reloadPage() {
+    window.location.reload();
+  }
+
+  return (
+    <div className={classNames(cls.PageError, {}, [className])}>
+      <p>{t('Произошла непредвиденная ошибка')}</p>
+      <Button theme={ThemeButton.CLEAR} onClick={() => reloadPage()}>
+        {t('Обновить страницу')}
+      </Button>
+    </div>
+
+  );
+}
