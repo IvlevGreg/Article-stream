3cb9e71585a89b8b75aaebda0893aca5f1be4608
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-19 11:48:53 +0300

    feat: create bug button component

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui';
+
+interface BugButtonProps {
+  className?: string;
+}
+
+// Компонент для тестирования ошибок ErrorBoundary
+
+export function BugButton(props: BugButtonProps) {
+  const { className } = props;
+  const [error, setError] = useState(false);
+  const { t } = useTranslation();
+
+  function toggleError() {
+    setError(!error);
+  }
+
+  function throwError() {
+    throw new Error(
+      'test error from bug button component (Компонент для тестирования ошибок ErrorBoundary)',
+    );
+  }
+
+  useEffect(() => {
+    if (error) throwError();
+  }, [error]);
+
+  return (
+    <Button theme={ThemeButton.CLEAR} onClick={() => toggleError()}>
+      {t('Бросить ошибку')}
+    </Button>
+  );
+}
