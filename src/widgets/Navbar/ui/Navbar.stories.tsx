2aefe225e6b4843ab5efd077284f5662e5c7e6ea
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-25 13:53:12 +0300

    refactoring: change  line endings from CRLF to LF
    CRLF (windows) LF (unix)

---
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Navbar } from './Navbar';
+
+export default {
+  title: 'widget/Navbar',
+  component: Navbar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Navbar>;
+
+const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {};
