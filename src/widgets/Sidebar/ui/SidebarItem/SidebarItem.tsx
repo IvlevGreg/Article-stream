101ffac9c0555c9b319007936c8b00e8e020bd3f
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-03-12 16:52:31 +0300

    feat: add memo for components

---
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -0,0 +1,31 @@
+import { AppLink, AppLinkTheme } from 'shared/ui';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { SidebarItemType } from '../../model/items';
+import cls from './SidebarItem.module.scss';
+
+interface SidebarItemProps {
+  collapsed: boolean;
+  item?: SidebarItemType;
+  className?: string;
+}
+
+export const SidebarItem = ({ className, collapsed, item }: SidebarItemProps) => {
+  const { t } = useTranslation();
+  const { Icon, path, text } = item;
+
+  return (
+
+    <AppLink
+      theme={AppLinkTheme.SECONDARY}
+      to={path}
+      className={classNames(cls.item, { [cls.collapsed]: collapsed }, [className])}
+    >
+      <Icon className={cls.icon} />
+      <span className={cls.link}>
+        {t(text)}
+      </span>
+    </AppLink>
+
+  );
+};
