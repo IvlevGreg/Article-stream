84f0d9faad335c6643149b9fd7f031bdb9376382
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-03-12 15:34:11 +0300

    feat: add public api for pages

--- src/shared/config/route/routeConfig.tsx
+++ src/shared/config/route/routeConfig.tsx
@@ -1,11 +1,16 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { NotFoundPage } from 'pages/NotFoundPage/ui/NotFoundPage';
 import { type RouteProps } from 'react-router-dom';
+import {
+  NotFoundPage, MainPage,
+  AboutPage,
+  ProfilePage,
+} from 'pages';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  PROFILE='profile',
+
+  // last
   NOTFOUND = 'notFound',
 
 }
@@ -13,11 +18,14 @@ export enum AppRoutes {
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.PROFILE]: '/profile',
   [AppRoutes.NOTFOUND]: '/*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: { path: RoutePath.main, element: <MainPage /> },
   [AppRoutes.ABOUT]: { path: RoutePath.about, element: <AboutPage /> },
+  [AppRoutes.PROFILE]: { path: RoutePath.profile, element: <ProfilePage /> },
+  // last
   [AppRoutes.NOTFOUND]: { path: RoutePath.notFound, element: <NotFoundPage /> },
 };
