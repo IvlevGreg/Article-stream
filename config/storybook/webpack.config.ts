ae9ec87af021d17dff08f3c7fc45e7f5fbfa1a1f
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-03-19 20:12:50 +0300

    feat:  add global variable __PROJECT__

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -36,6 +36,7 @@ export default ({ config }:{config:WebpackConfiguration}) => {
   config.plugins!.push(new webpack.DefinePlugin({
     __IS_DEV__: JSON.stringify(true),
     __API__: JSON.stringify(''),
+    __PROJECT__: JSON.stringify('storybook'),
   }));
 
   config.resolve!.modules = [
