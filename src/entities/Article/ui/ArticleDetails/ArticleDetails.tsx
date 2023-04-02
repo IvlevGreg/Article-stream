53304ad328b5391dbb3f01a59f41003c3e98f1dc
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-04-02 16:22:00 +0300

    feat: create ArticleDetails component

---
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -0,0 +1,137 @@
+import { useTranslation } from 'react-i18next';
+import {
+  FC, memo, useCallback, useEffect,
+} from 'react';
+
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/lib/hooks';
+import { useSelector } from 'react-redux';
+import {
+  getArticleDetailsData,
+  getArticleDetailsError,
+  getArticleDetailsIsLoading,
+} from 'entities/Article/model/selectors/articleDetails';
+import {
+  TextAlign, TextSize, Icon, Avatar, Skeleton, Text,
+} from 'shared/ui';
+import { classNames } from 'shared/lib/classNames/classNames';
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import CalendarIcon from 'shared/assets/icons/calendar-20-20.svg';
+import { ArticleBlock, ArticleBlockType } from 'entities/Article/model/types/article';
+import cls from './ArticleDetails.module.scss';
+import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
+import { fetchArticleById } from '../../model/services/fetchArticleById';
+import { ArticleCodeBlockComponent } from '../ArticleCodeBlockComponent/ArticleCodeBlockComponent';
+import { ArticleImageBlockComponent } from '../ArticleImageBlockComponent/ArticleImageBlockComponent';
+import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
+
+const reducers:ReducersList = {
+  articleDetails: articleDetailsReducer,
+
+};
+
+export interface ArticleDetailsProps {
+  className?: string;
+  id:string;
+}
+
+export const ArticleDetails: FC<ArticleDetailsProps> = ({ className, id }) => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+
+  const isLoading = useSelector(getArticleDetailsIsLoading);
+  const error = useSelector(getArticleDetailsError);
+  const article = useSelector(getArticleDetailsData);
+
+  useEffect(() => {
+    dispatch(fetchArticleById(id));
+  }, [dispatch, id]);
+
+  let content;
+
+  const renderBlock = useCallback((block: ArticleBlock) => {
+    switch (block.type) {
+    case ArticleBlockType.CODE:
+      return (
+        <ArticleCodeBlockComponent
+          key={block.id}
+          block={block}
+          className={cls.block}
+        />
+      );
+    case ArticleBlockType.IMAGE:
+      return (
+        <ArticleImageBlockComponent
+          key={block.id}
+          block={block}
+          className={cls.block}
+        />
+      );
+    case ArticleBlockType.TEXT:
+      return (
+        <ArticleTextBlockComponent
+          key={block.id}
+          className={cls.block}
+          block={block}
+        />
+      );
+    default:
+      return null;
+    }
+  }, []);
+
+  if (isLoading) {
+    content = (
+      <>
+        <Skeleton className={cls.avatar} width={200} height={200} border="50%" />
+        <Skeleton className={cls.title} width={300} height={32} />
+        <Skeleton className={cls.skeleton} width={600} height={24} />
+        <Skeleton className={cls.skeleton} width="100%" height={200} />
+        <Skeleton className={cls.skeleton} width="100%" height={200} />
+      </>
+    );
+  } else if (error) {
+    content = (
+      <Text
+        align={TextAlign.CENTER}
+        title={t('Произошла ошибка при загрузке статьи.')}
+      />
+    );
+  } else {
+    content = (
+      <>
+        <div className={cls.avatarWrapper}>
+          <Avatar
+            size={200}
+            src={article?.img}
+            className={cls.avatar}
+          />
+        </div>
+        <Text
+          className={cls.title}
+          title={article?.title}
+          text={article?.subtitle}
+          size={TextSize.L}
+        />
+        <div className={cls.articleInfo}>
+          <Icon className={cls.icon} Svg={EyeIcon} />
+          <Text text={String(article?.views)} />
+        </div>
+        <div className={cls.articleInfo}>
+          <Icon className={cls.icon} Svg={CalendarIcon} />
+          <Text text={article?.createdAt} />
+        </div>
+        {article?.blocks.map(renderBlock)}
+      </>
+    );
+  }
+
+  return (
+    <DynamicModuleLoader reducers={reducers}>
+
+      <div className={classNames(cls.ArticleDetails, {}, [className])}>
+        {content}
+      </div>
+    </DynamicModuleLoader>
+  );
+};
---
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -0,0 +1,25 @@
+import { ArticleImageBlock } from 'entities/Article/model/types/article';
+import { Text, TextAlign } from 'shared/ui';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './ArticleImageBlockComponent.module.scss';
+
+interface ArticleImageBlockComponentProps {
+    className?: string;
+    block: ArticleImageBlock;
+}
+
+export const ArticleImageBlockComponent = memo((props: ArticleImageBlockComponentProps) => {
+  const { className, block } = props;
+
+  return (
+    <div className={classNames(cls.ArticleImageBlockComponent, {}, [className])}>
+      <img src={block.src} alt={block.title} className={cls.img} />
+      {block.title && (
+        <Text text={block.title} align={TextAlign.CENTER} />
+      )}
+    </div>
+  );
+});
